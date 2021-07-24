fb09a438f029b4e8802aa4109ed9d8bee53c5033
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2021-07-24 13:28:31 +0500

    styles for section title

--- js/common.js
+++ js/common.js
@@ -1,42 +1,5 @@
 jQuery(function($) {
 
-	let wind = $(window);
-
-	function stickyHeader(){
-		let windowWidth = wind.width();
-		let h_hght = 32;
-		let h_mrg = 0;
-		let elem = $('#header');
-		let top = elem.scrollTop();
-
-		if (windowWidth < 992)
-			h_hght = 78;          
-
-		if (windowWidth > 575){
-			if(top > h_hght){
-				elem.css('top', h_mrg);
-			}else {
-				elem.css('top', h_hght);
-			}
-			wind.scroll(function(){
-				top = $(this).scrollTop();
-				if (top+h_mrg < h_hght) {
-					elem.css('top', (h_hght-top));
-				} else {
-					elem.css('top', h_mrg);
-				}
-			});
-		}else{
-			elem.css('top', h_mrg);
-			wind.scroll(function(){
-				elem.css('top', h_mrg);
-			});
-		}		
-	}
-
-	stickyHeader();
-	wind.on('resize', function(){stickyHeader()});
-
 	$('.top').click(function() {
 		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 	});
